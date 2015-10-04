c7c4897d7675ab0840f682ac582e75176ca93202
Adam Ringwood <aringwoo@gmail.com>
2015-10-04 10:41:50 +0000

    adding twitter

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
+// Twitter
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
 // create a new express server
 var app = express();
 
@@ -34,4 +45,12 @@ app.get('/test', function (req, res) {
 
 app.get('/tweets', function (req, res) {
   res.send('Here is where the tweets would be, if we had any!');
+  
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+	if (!error) {
+		console.log(tweets);
+	}
+  });
+
 });
