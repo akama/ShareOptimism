7bf40b1344883ef21f3253b0265572145f367b5e
Adam Ringwood <adam@nexadmin.com>
2015-10-04 15:09:17 +0200

    hooking in front end

--- public/js/semantic-beta.js
+++ public/js/semantic-beta.js
@@ -1,11 +1,14 @@
 ( function () {
-    var dataUrl = 'http://genre-classifier.eu-gb.mybluemix.net/test?userid=';
     $("#twitter_btn").on("click", function(event) {
+	var dataUrl = '/watson?userid=';
         var userName = $("#username").val();
         dataUrl += userName;
         console.log(userName);
         $.getJSON(dataUrl)
          .done(function(data){
+	    $("#pos_bar").css("width", String(Math.floor(100*(data["positive"]/data["total"]))) + "%")
+	    $("#new_bar").css("width", String(Math.floor(100*(data["neutral"]/data["total"]))) + "%")
+	    $("#neg_bar").css("width", String(Math.floor(100*(data["negative"]/data["total"]))) + "%")
             console.log(data);
             $("#result").html(data.user);        
        });   
