1b659c6796aaaf79ab1aa121b71eb8cfeada69f4
Emanuel Lönnberg <emanuel@aceromonto.com>
2015-10-04 14:13:46 +0200

    minor changes param

--- public/js/semantic-beta.js
+++ public/js/semantic-beta.js
@@ -1,10 +1,12 @@
 ( function () {
-    var dataUrl = 'http://genre-classifier.eu-gb.mybluemix.net/test?userid="emanuel"';
+    var dataUrl = 'http://genre-classifier.eu-gb.mybluemix.net/test?userid=';
     $("#twitter_btn").on("click", function(event) {
         var userName = $("#username").val();
+        dataUrl += userName;
         console.log(userName);
         $.get(dataUrl)
          .done(function(data){
+            console.log(data);
             $("#result").html(data);        
        });   
     });
