d802c6e0ad885d87c4f58311d2bbcb232b0f784d
Adam Ringwood <adam@nexadmin.com>
2015-10-04 15:54:32 +0200

    adding percents to the front page

--- public/js/semantic-beta.js
+++ public/js/semantic-beta.js
@@ -1,17 +1,23 @@
 ( function () {
-    $("#twitter_btn").on("click", function(event) {
-	var dataUrl = '/watson?userid=';
-        var userName = $("#username").val();
-        dataUrl += userName;
-        console.log(userName);
-        $.getJSON(dataUrl)
-         .done(function(data){
-	    $("#pos_bar").css("width", String(Math.floor(100*(data["positive"]/data["total"]))) + "%")
-	    $("#new_bar").css("width", String(Math.floor(100*(data["neutral"]/data["total"]))) + "%")
-	    $("#neg_bar").css("width", String(Math.floor(100*(data["negative"]/data["total"]))) + "%")
-            console.log(data);
-            $("#result").html(data.user);        
-       });   
-    });
+	$("#twitter_btn").on("click", function(event) {
+		var dataUrl = '/watson?userid=';
+		var userName = $("#username").val();
+		dataUrl += userName;
+		console.log(userName);
+		$.getJSON(dataUrl)
+			.done(function(data){
+				var pos_percent = Math.floor(100*(data["positive"]/data["total"]));
+				var new_percent = Math.floor(100*(data["neutral"]/data["total"]));
+				var neg_percent = Math.floor(100*(data["negative"]/data["total"]));
+				$("#pos_text").text("Positive: " + String(pos_percent) + "%");
+				$("#new_text").text("Neutral: " + String(new_percent) + "%");
+				$("#neg_text").text("Negative: " + String(neg_percent) + "%");
+				$("#pos_bar").css("width", String(pos_percent) + "%");
+				$("#new_bar").css("width", String(new_percent) + "%");
+				$("#neg_bar").css("width", String(neg_percent) + "%");
+				console.log(data);
+				$("#result").html(data.user);
+			});
+	});
 
 })()
