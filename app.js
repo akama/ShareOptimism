3d7e5b3cbe0f1be20f80ac5bf9b913858d053cd4
Emanuel Lönnberg <emanuel@aceromonto.com>
2015-10-04 10:09:11 +0000

    adding /test as a test endpoint

--- app.js
+++ app.js
@@ -27,3 +27,7 @@ app.listen(appEnv.port, function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+app.get('/test', function (req, res) {
+  res.send('Hello World!');
+});
