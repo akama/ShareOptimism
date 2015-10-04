7ea6b3f514113d3323e5ad10e8c7ae04d7fec624
Adam Ringwood <adam@nexadmin.com>
2015-10-04 14:20:34 +0200

    added end point

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ var client = new Twitter({
 // Watson
 var watson = require('watson-developer-cloud');
 
+var natural_language_classifier = watson.natural_language_classifier({
+	url: 'https://gateway.watsonplatform.net/natural-language-classifier/api',
+	username: process.env.NLP_USER,
+	password: process.env.NLP_PASS,
+	version: 'v1'
+});
+
+// Alchemy Language
+var alchemy_language = watson.alchemy_language({
+	  api_key: process.env.ALCHEMY_API
+});
+
 // create a new express server
 var app = express();
 
@@ -46,15 +58,37 @@ app.get('/test', function (req, res) {
   res.send({user:req.param('userid')});
 });
 
-app.get('/tweets', function (req, res) {
-  var params = {screen_name: 'nodejs'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response){
+app.get('/watson', function (req, res) {
+	tweet_storage = {"total": 0, "positive": 0, "negative": 0, "neutral": 0}
+	
+	var return_results = function (output) {
+		res.send(output)
+	}
+
+	var output = function (results, total_number) {
+		console.log(results);
+		tweet_storage["total"] += 1;
+		tweet_storage[results["type"]] += 1;
+		if (tweet_storage["total"] == total_number) {
+			console.log(tweet_storage);
+			return_results(tweet_storage);
+		}
+	};
+
+	var params = {screen_name: req.param('userid')};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
 	if (!error) {
-		res.send(tweets);
+		for (var i = 0; i < tweets.length; i++) {
+			alchemy_language.sentiment({"text": tweets[i]["text"]}, function (err, response) {
+			  if (err)
+			    console.log('error:', err);
+			  else
+			    output(response["docSentiment"], tweets.length);
+			}); 
+		}
 	} else {
 		console.log(error);
 		res.send('Something went horrible wrong, I blame solar flares.');
 	}
-  });
-
+	});
 });
